0be96b546df7f5208d8aae8290530d0e7964a2dc
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-07-02 02:34:53 +0500

    farq

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let arr = [1 , 2 , 3 , 4 , 5 , 6 , 7]
+let first_item = arr[0]
+let last_item = arr[6]
+if (first_item > last_item){
+    alert("Arraydagi 0 chi indexi katta")
+} else if (last_item > first_item){
+    alert("Arrayni 6 chi indexi katta")
+}
